357435619ebc8919a638535bdba218b31fbd8278
admin <admin@20210277-RAJIB>
2021-12-18 13:24:26 +1300

    Add project files.

---
+++ FileUploader/Form1.Designer.cs
@@ -0,0 +1,151 @@
+﻿
+namespace FileUploader
+{
+    partial class FileUploader
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txt_fileName = new System.Windows.Forms.TextBox();
+            this.btn_select = new System.Windows.Forms.Button();
+            this.uploadPanel = new System.Windows.Forms.Panel();
+            this.btn_upload = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.chk_AZURE = new System.Windows.Forms.CheckBox();
+            this.chk_FTP = new System.Windows.Forms.CheckBox();
+            this.uploadPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txt_fileName
+            // 
+            this.txt_fileName.Location = new System.Drawing.Point(134, 85);
+            this.txt_fileName.Name = "txt_fileName";
+            this.txt_fileName.Size = new System.Drawing.Size(311, 23);
+            this.txt_fileName.TabIndex = 0;
+            this.txt_fileName.Text = "File Name";
+            // 
+            // btn_select
+            // 
+            this.btn_select.Location = new System.Drawing.Point(475, 85);
+            this.btn_select.Name = "btn_select";
+            this.btn_select.Size = new System.Drawing.Size(129, 23);
+            this.btn_select.TabIndex = 1;
+            this.btn_select.Text = "Select File";
+            this.btn_select.UseVisualStyleBackColor = true;
+            this.btn_select.Click += new System.EventHandler(this.btn_select_Click);
+            // 
+            // uploadPanel
+            // 
+            this.uploadPanel.Controls.Add(this.btn_upload);
+            this.uploadPanel.Controls.Add(this.label2);
+            this.uploadPanel.Controls.Add(this.label1);
+            this.uploadPanel.Controls.Add(this.chk_AZURE);
+            this.uploadPanel.Controls.Add(this.chk_FTP);
+            this.uploadPanel.Location = new System.Drawing.Point(119, 152);
+            this.uploadPanel.Name = "uploadPanel";
+            this.uploadPanel.Size = new System.Drawing.Size(503, 228);
+            this.uploadPanel.TabIndex = 2;
+            this.uploadPanel.Visible = false;
+            // 
+            // btn_upload
+            // 
+            this.btn_upload.Location = new System.Drawing.Point(356, 181);
+            this.btn_upload.Name = "btn_upload";
+            this.btn_upload.Size = new System.Drawing.Size(129, 23);
+            this.btn_upload.TabIndex = 3;
+            this.btn_upload.Text = "Upload FIle";
+            this.btn_upload.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(14, 23);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(115, 21);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "File upload to";
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 23);
+            this.label1.TabIndex = 0;
+            // 
+            // chk_AZURE
+            // 
+            this.chk_AZURE.AutoSize = true;
+            this.chk_AZURE.Location = new System.Drawing.Point(36, 103);
+            this.chk_AZURE.Name = "chk_AZURE";
+            this.chk_AZURE.Size = new System.Drawing.Size(56, 19);
+            this.chk_AZURE.TabIndex = 1;
+            this.chk_AZURE.Text = "Azure";
+            this.chk_AZURE.UseVisualStyleBackColor = true;
+            // 
+            // chk_FTP
+            // 
+            this.chk_FTP.AutoSize = true;
+            this.chk_FTP.Location = new System.Drawing.Point(36, 60);
+            this.chk_FTP.Name = "chk_FTP";
+            this.chk_FTP.Size = new System.Drawing.Size(80, 19);
+            this.chk_FTP.TabIndex = 0;
+            this.chk_FTP.Text = "FTP Server";
+            this.chk_FTP.UseVisualStyleBackColor = true;
+            // 
+            // FileUploader
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.uploadPanel);
+            this.Controls.Add(this.btn_select);
+            this.Controls.Add(this.txt_fileName);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "FileUploader";
+            this.Text = "File Uploader";
+            this.uploadPanel.ResumeLayout(false);
+            this.uploadPanel.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txt_fileName;
+        private System.Windows.Forms.Button btn_select;
+        private System.Windows.Forms.Panel uploadPanel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckBox chk_AZURE;
+        private System.Windows.Forms.CheckBox chk_FTP;
+        private System.Windows.Forms.Button btn_upload;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ FileUploader/Form1.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Net;
+
+
+using Microsoft.Azure;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+
+namespace FileUploader
+{
+    public partial class FileUploader : Form
+    {
+        public FileUploader()
+        {
+            InitializeComponent();
+            txt_fileName.Enabled = false;
+        }
+
+        CloudStorageAccount storageAccount = null;
+        CloudBlobContainer cloudBlobContainer = null;
+
+        private void btn_select_Click(object sender, EventArgs e)
+        {
+            //hide the upload panel, incase it is visible
+            uploadPanel.Visible = false;
+            
+            //opening file dialog
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.CheckFileExists = true;
+            openFileDialog.AddExtension = true;
+            openFileDialog.Multiselect = false;
+            //filtering files (only csv & xlsx)
+            openFileDialog.Filter = "Excel files (*.csv or *.xlsx)|*.csv;*.xlsx";
+
+
+            if (openFileDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                MessageBox.Show(openFileDialog.FileName);
+
+                if (openFileDialog.FileName != "")
+                {
+                    txt_fileName.Text = Path.GetFileName(openFileDialog.FileName);
+                    uploadPanel.Visible = true;
+                    //  uploadToFTP(openFileDialog.FileName.ToString());
+
+                    uploadToAzure(openFileDialog.FileName.ToString());
+                    
+                }
+                else
+                {
+                   
+
+                }
+
+                foreach (string fileName in openFileDialog.FileNames)
+                {
+                    //Process.Start(fileName);
+                }
+            }
+        }
+
+
+        private void uploadToFTP(string filePath)
+        {
+            //using (var ftpServer = new WebClient())
+            //{
+            //    ftpServer.Credentials = new NetworkCredential("b3_30610330", "Sh@rpC0d3r");
+
+            //   ftpServer.UploadFile( new Uri("ftp://ftp.byethost3.com/htdocs"), WebRequestMethods.Ftp.UploadFile, filePath);
+            //} 
+
+            //using (WebClient webClient = new WebClient())
+            //{
+            //    webClient.Credentials = new NetworkCredential("b3_30610330", "Sh@rpC0d3r");
+            //    await webClient.UploadFileTaskAsync(new Uri("ftp://ftp.byethost3.com/htdocs"), WebRequestMethods.Ftp.UploadFile, filePath);
+            //}
+
+            FtpWebRequest request = (FtpWebRequest)WebRequest.Create(new Uri(string.Format("{0}/{1}", "ftp://ftp.byethost3.com/htdocs", Path.GetFileName(filePath))));
+
+            request.Method = WebRequestMethods.Ftp.UploadFile;
+            request.Credentials = new NetworkCredential("b3_30610330", "Sh@rpC0d3r");
+
+            Stream ftpStream = request.GetRequestStream();
+            FileStream fs = File.OpenRead(filePath);
+            byte[] buffer = new byte[32];
+            double total = (double)fs.Length;
+            int byteRead = 0;
+            double read = 0;
+            do
+            {
+                byteRead = fs.Read(buffer);
+                ftpStream.Write(buffer, 0, byteRead);
+                read += (double)byteRead;
+                double percentage = read / total * 100;
+            } while (byteRead != 0);
+            fs.Close();
+            ftpStream.Close();
+
+
+
+        }
+
+
+        private async void uploadToAzure(string filePath)
+
+        {
+            string connectionString = "DefaultEndpointsProtocol=https;AccountName=fccdemo1;AccountKey=mCSLo6Lz3rn3INbO69orMHhhgn29nqiKbtEATrDP71OCPiiNf3KIIKJkS0+bXaaT3fKivqBRzj6d5FLhFWpMTg==;EndpointSuffix=core.windows.net";
+
+            //string storageConnection = CloudConfigurationManager.GetSetting("DefaultEndpointsProtocol=https;AccountName=fccdemo1;AccountKey=mCSLo6Lz3rn3INbO69orMHhhgn29nqiKbtEATrDP71OCPiiNf3KIIKJkS0+bXaaT3fKivqBRzj6d5FLhFWpMTg==;EndpointSuffix=core.windows.net"); 
+            //CloudStorageAccount cloudStorageAccount = CloudStorageAccount.Parse(storageConnection);
+
+
+
+            ////create a block blob
+            //CloudBlobClient cloudBlobClient = cloudStorageAccount.CreateCloudBlobClient();
+
+            ////create a container
+            //CloudBlobContainer cloudBlobContainer = cloudBlobClient.GetContainerReference("appcontainer");
+
+            ////create a container if it is not already exists
+
+            //if (await cloudBlobContainer.CreateIfNotExistsAsync())
+            //{
+
+            //    await cloudBlobContainer.SetPermissionsAsync(new BlobContainerPermissions { PublicAccess = BlobContainerPublicAccessType.Blob });
+
+            //}
+
+            ////string imageName = "Test-" + Path.GetExtension(openFileDialog.FileName);
+
+            ////get Blob reference
+
+            //CloudBlockBlob cloudBlockBlob = cloudBlobContainer.GetBlockBlobReference(Path.GetFileName(filePath)); 
+            ////cloudBlockBlob.Properties.ContentType = openFileDialog.ContentType;
+
+            //await cloudBlockBlob.UploadFromFileAsync(filePath);
+
+            if (CloudStorageAccount.TryParse(connectionString, out storageAccount))
+            {
+
+                CloudBlobClient cloudBlobClient = storageAccount.CreateCloudBlobClient();
+               
+               
+                cloudBlobContainer = cloudBlobClient.GetContainerReference("fccstorage");
+               
+                //if (cloudBlobContainer != null)
+                //{
+                //    MessageBox.Show("Null");
+                //    return;
+                //}
+                //if the container do not exists create it. 
+               // cloudBlobContainer = cloudBlobClient.GetContainerReference("fccstorage" + Guid.NewGuid().ToString());
+              //  await cloudBlobContainer.CreateAsync();
+              
+
+                BlobContainerPermissions permissions = new BlobContainerPermissions
+                {
+                    PublicAccess = BlobContainerPublicAccessType.Blob
+                };
+                await cloudBlobContainer.SetPermissionsAsync(permissions);
+            }
+
+            CloudBlockBlob cloudBlockBlob = cloudBlobContainer.GetBlockBlobReference(Path.GetFileName(filePath));
+            await cloudBlockBlob.UploadFromFileAsync(filePath);
+
+
+        }
+    }
+}
